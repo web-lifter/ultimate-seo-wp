4dc943d79cf0d826876d4f1a70b35d7a935594e1
johnoconnor0 <john@weblifter.com.au>
2025-03-21 17:19:04 +1000

    Updates to updater

--- ultimate-seo-wp.php
+++ ultimate-seo-wp.php
@@ -23,6 +23,11 @@ define('ULTIMATE_SEO_PLUGIN_URL', plugin_dir_url(__FILE__));
 require_once ULTIMATE_SEO_PLUGIN_DIR . 'admin/settings.php';
 
 /**
+ * Load Updater
+ */
+include_once plugin_dir_path(__FILE__) . 'updater/github-updater.php';
+
+/**
  * Conditionally Load Sitemap Functionality
  */
 if (get_option('ultimate_seo_enable_sitemap', 1)) {
@@ -63,31 +68,3 @@ function ultimate_seo_enqueue_admin_scripts($hook) {
     }
 }
 add_action('admin_enqueue_scripts', 'ultimate_seo_enqueue_admin_scripts');
-
-function ultimate_seo_check_for_updates( $transient ) {
-    if ( empty( $transient->checked ) ) {
-        return $transient;
-    }
-
-    $update_url = plugins_url( 'updater.php', __FILE__ );
-    $remote = wp_remote_get( $update_url );
-
-    if ( is_wp_error( $remote ) ) {
-        return $transient;
-    }
-
-    $response = json_decode( wp_remote_retrieve_body( $remote ) );
-
-    if ( version_compare( '1.0.0', $response->version, '<' ) ) {
-        $transient->response['ultimate-seo-wp/my-plugin.php'] = (object) array(
-            'slug'        => 'ultimate-seo-wp',
-            'new_version' => $response->version,
-            'package'     => $response->download_url,
-            'url'         => $response->homepage
-        );
-    }
-
-    return $transient;
-}
-add_filter( 'site_transient_update_plugins', 'ultimate_seo_check_for_updates' );
-
