eb438bae31d57effd86c46709378040710b9ff28
johnoconnor0 <john@weblifter.com.au>
2025-03-21 16:56:47 +1000

    Automatic Updates

--- ultimate-seo-wp.php
+++ ultimate-seo-wp.php
@@ -2,9 +2,11 @@
 /**
  * Plugin Name: Ultimate SEO WP
  * Description: A lightweight and efficient SEO plugin for WordPress.
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Web Lifter
+ * Author URI: https://weblifter.com.au
  * License: GPL2
+ * Update URI: /updates.json
  */
 
 if (!defined('ABSPATH')) {
@@ -61,3 +63,31 @@ function ultimate_seo_enqueue_admin_scripts($hook) {
     }
 }
 add_action('admin_enqueue_scripts', 'ultimate_seo_enqueue_admin_scripts');
+
+function ultimate_seo_check_for_updates( $transient ) {
+    if ( empty( $transient->checked ) ) {
+        return $transient;
+    }
+
+    $update_url = plugins_url( 'updater.php', __FILE__ );
+    $remote = wp_remote_get( $update_url );
+
+    if ( is_wp_error( $remote ) ) {
+        return $transient;
+    }
+
+    $response = json_decode( wp_remote_retrieve_body( $remote ) );
+
+    if ( version_compare( '1.0.0', $response->version, '<' ) ) {
+        $transient->response['ultimate-seo-wp/my-plugin.php'] = (object) array(
+            'slug'        => 'ultimate-seo-wp',
+            'new_version' => $response->version,
+            'package'     => $response->download_url,
+            'url'         => $response->homepage
+        );
+    }
+
+    return $transient;
+}
+add_filter( 'site_transient_update_plugins', 'ultimate_seo_check_for_updates' );
+
